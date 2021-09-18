5332ad886827670612f00f9edd5f2219b8347648
Serhii Saunin <serhiisaunin@gmail.com>
2021-09-19 02:18:46 +0300

    Gulp-Update

--- gulpfile.js
+++ gulpfile.js
@@ -6,73 +6,82 @@ const browserSync  = require('browser-sync').create(),
       sass         = require('gulp-sass')(require('sass')),
       autoprefixer = require('gulp-autoprefixer'),
       imagemin     = require('gulp-imagemin'),
-      newer        = require('gulp-newer'),
+      changed      = require('gulp-changed'),
       del          = require('del');
 
-const bs = () => {
+const server = () => {
    browserSync.init ({
    server: {baseDir: 'app/'},
    notify: false,
    online: true
    })
-}
+};
 
 const styles = () => {
    return src('app/scss/style.scss')
    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
    .pipe(concat('style.min.css'))
    .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
-   .pipe(dest('app/css/'))
+   .pipe(dest('app/css'))
    .pipe(browserSync.stream())
-}
+};
 
 const scripts = () => {
-   return src([
-      'app/js/script.js',
-   ])
-   .pipe(concat('scripts.min.js'))
+   return src('app/js/src/script.js' )
+   .pipe(concat('script.min.js'))
    .pipe(uglify())
-   .pipe(dest('app/js'))
+   .pipe(dest('app/js/dest'))
    .pipe(browserSync.stream())
-}
+};
 
 const images = () => {
    return src('app/img/src/**/*')
-   .pipe(newer('app/img/dest/'))
-   .pipe(imagemin())
-   .pipe(dest('app/img/dest/'))
-}
+   .pipe(changed('app/img/dest'))
+   .pipe(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.svgo({
+          plugins: [
+              {removeViewBox: true},
+              {cleanupIDs: false}
+          ]
+      })
+  ]))
+   .pipe(dest('app/img/dest'))
+};
 
-const cleanimg = () => {
+const cleanImg = () => {
 	return del('app/img/dest/**/*', { force: true }) 
-}
+};
 
-const buildCopy = () => {
-	return src([
+const buildDist = () => {
+   return src([
       'app/css/**/*.min.css',
       'app/js/**/*.min.js',
       'app/img/dest/**/*',
       'app/**/*.html'
-	], {base: 'app'})
+   ], {base: 'app'})
    .pipe(dest('dist'))
-}
+};
 const cleanBuild =() => {
    return del('dist/**/*', { force: true }) 
-}
+};
 
 const startWatch = () => {
    watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
-   watch(['app/**/*.scss'], styles);
+   watch('app/**/*.scss', styles);
    watch('app/**/*.html').on('change', browserSync.reload);
    watch('app/img/src/**/*', images);
-}
+};
 
-exports.bs         = bs;
+exports.server     = server;
 exports.scripts    = scripts;
 exports.styles     = styles;
 exports.images     = images;
-exports.cleanimg   = cleanimg;
+exports.buildDist = buildDist;
+exports.cleanImg   = cleanImg;
 exports.cleanBuild = cleanBuild;
-exports.build      = series(cleanBuild, styles, scripts, images, buildCopy);
-exports.default    = parallel( scripts, bs, startWatch);
+exports.build      = series(cleanBuild, styles, scripts, images, buildDist);
+exports.default    = parallel(server,styles, scripts, startWatch);
 
